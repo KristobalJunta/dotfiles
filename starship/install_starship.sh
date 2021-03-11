c66e3943b7d77565dc8c6bc4bcb6fc32a8aeb83e
Eugene Tverdokhleb <junta.kristobal@gmail.com>
2021-03-12 00:17:43 +0200

    update starship, migrate config to 0.45+

--- starship/install_starship.sh
+++ starship/install_starship.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 # Installs starship.rs command prompt if it's not already there
 
-if [ -z `command -v starship` ]; then
+if [ -z "$(command -v starship)" ]; then
     curl -fsSL https://starship.rs/install.sh > starship_installer.sh
     bash starship_installer.sh -b "$HOME/bin" -f
     rm starship_installer.sh
