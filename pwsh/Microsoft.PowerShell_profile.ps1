314e201f1438cc52496b7d6978b1c603e35148f2
Eugene <junta.kristobal@gmail.com>
2024-12-20 18:31:27 +0200

    update PROMPT_EMOJI, sync bash and pwsh profiles

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -6,10 +6,14 @@ clear
 
 Switch (( get-date ).DayOfWeek)
 {
+    'Monday'    { $env:PROMPT_EMOJI = '✨✨✨' }
+    'Tuesday'   { $env:PROMPT_EMOJI = '🐄🐄🐄' }
     'Wednesday' { $env:PROMPT_EMOJI = '🐸🐸🐸' }
     'Thursday'  { $env:PROMPT_EMOJI = '🐟🐟🐟' }
     'Friday'    { $env:PROMPT_EMOJI = '🍺🍺🍺' }
-    default     { $env:PROMPT_EMOJI = '🚀🚀🚀' }
+    'Saturday'  { $env:PROMPT_EMOJI = '🛋️🛋️🛋️' }
+    'Sunday'    { $env:PROMPT_EMOJI = '☀️☀️☀️' }
+    default     { $env:PROMPT_EMOJI = '✨✨✨' }
 }
 
-oh-my-posh init pwsh --config ~\Documents\PowerShell\kristobaljunta_compact.omp.json | Invoke-Expression
+Invoke-Expression (&starship init powershell)
