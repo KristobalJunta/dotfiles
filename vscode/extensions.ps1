5021646078e6f026f158a1c2642126cc5e0ce05d
Eugene <junta.kristobal@gmail.com>
2025-11-18 18:32:25 +0200

    add vscode configs

---
+++ vscode/extensions.ps1
@@ -0,0 +1,20 @@
+# code --list-extensions | % { "code --install-extension $_" }
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
+code --install-extension artisanbytecrafter.poptheme
+code --install-extension beardedbear.beardedicons
+code --install-extension codezombiech.gitignore
+code --install-extension dakara.transformer
+code --install-extension editorconfig.editorconfig
+code --install-extension metalbear.mirrord
+code --install-extension mikestead.dotenv
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.vscode-python-envs
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode.hexeditor
+code --install-extension redhat.vscode-yaml
+code --install-extension tomoki1207.selectline-statusbar
+code --install-extension tyriar.sort-lines
+
